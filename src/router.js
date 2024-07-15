2406304ec1fa390715326bb5fb6979796cd62bd8
homonoviscoding <mama.leye@gmail.com>
2024-07-15 23:17:04 +0100

    implemented the POST req for handing token to client

--- src/router.js
+++ src/router.js
@@ -14,7 +14,22 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body
 
+    try {
+
+        if (username !== mockUser.username || password !== mockUser.password) {
+            return res.status(401).json({ error: 'username or password incorrect, try again!' })
+        }
+        else {
+            const payload = { username: mockUser.username }
+            const secret = process.env.JWT_SECRET
+            const token = jwt.sign((payload), secret, { algorithm: 'HS256' })
+            return res.status(200).json({ token: token })
+        }
+    } catch (error) {
+        console.error(error)
+    }
 });
 
 router.get('/profile', (req, res) => {
